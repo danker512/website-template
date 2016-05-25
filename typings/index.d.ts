3daf8e2b8a5510a04c6f21e73f8bb2d47bb32413
TSHiYK <toshiyuki.1223+dev@gmail.com>
2016-05-25 16:18:06 +0900

    typingsをリポジトリから削除

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/bootstrap/index.d.ts" />
-/// <reference path="globals/dw-bxslider-4/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
