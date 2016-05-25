0ed9bdea245e9fef2eaddc9b7e159e7df711fca2
TSHiYK <toshiyuki.1223+dev@gmail.com>
2016-05-25 16:32:20 +0900

    Revert "setup.shを修正"
    This reverts commit acc2fa27e30facc31c9793bee647adb6f47393f3.

--- setup.sh
+++ setup.sh
@@ -15,6 +15,13 @@ if [ $? -ne 0 ] ; then
 	ISERROR=1
 fi
 
+which typescript > /dev/null 2>&1
+if [ $? -ne 0 ] ; then
+	echo "command not found: typescript"
+	echo "please install typescript. e.g. npm install -g typescript"
+	ISERROR=1
+fi
+
 which typings > /dev/null 2>&1
 if [ $? -ne 0 ] ; then
 	echo "command not found: typings"
