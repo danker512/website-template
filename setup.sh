1293f89b906a4f45197c687b0097c2856d852a0f
TSHiYK <toshiyuki.1223+dev@gmail.com>
2016-05-25 16:24:45 +0900

    setup.shを追加

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+ISERROR=0
+
+which npm > /dev/null 2>&1
+if [ $? -ne 0 ] ; then
+	echo "command not found: npm"
+	echo "please install npm. e.g. sudo port install npm"
+	ISERROR=1
+fi
+
+which gulp > /dev/null 2>&1
+if [ $? -ne 0 ] ; then
+	echo "command not found: gulp"
+	echo "please install gulp. e.g. npm install -g gulp"
+	ISERROR=1
+fi
+
+which typescript > /dev/null 2>&1
+if [ $? -ne 0 ] ; then
+	echo "command not found: typescript"
+	echo "please install typescript. e.g. npm install -g typescript"
+	ISERROR=1
+fi
+
+which typings > /dev/null 2>&1
+if [ $? -ne 0 ] ; then
+	echo "command not found: typings"
+	echo "please install typings. e.g. npm install -g typings"
+	ISERROR=1
+fi
+
+if [ $ISERROR == 1 ] ; then
+	exit
+fi
+
+rm -rf node_modules typings
+npm install
+typings install
+
+echo "COMPLETE!!"
