2e7efcf117c9746c8f4f3f648aad98c8e7e2cf4a
TSHiYK <toshiyuki.1223+dev@gmail.com>
2016-06-18 10:56:51 +0900

    Fix setup.sh
    Git flowを追加。

--- setup.sh
+++ setup.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 ISERROR=0
 
+which git-flow > /dev/null 2>&1
+if [ $? -ne 0 ] ; then
+	echo "command not found: git-flow"
+	echo "please install npm. e.g. brew install git-flow"
+	ISERROR=1
+fi
+
+# eaglesakura/git-flow-hook
+# https://github.com/eaglesakura/git-flow-hook
+which git-flow-fook > /dev/null 2>&1
+if [ $? -ne 0 ] ; then
+	echo "command not found: git-flow-hook"
+	echo "please install git-flow-hook. see https://github.com/eaglesakura/git-flow-hook"
+	ISERROR=1
+fi
+
 which npm > /dev/null 2>&1
 if [ $? -ne 0 ] ; then
 	echo "command not found: npm"
@@ -33,8 +49,21 @@ if [ $ISERROR == 1 ] ; then
 	exit
 fi
 
-rm -rf node_modules typings
+
+# ##############################
+# remove
+# ##############################
+rm -rf node_modules typings .git
+
+# ##############################
+# install
+# ##############################
 npm install
 typings install
+echo "INSTALL COMPLETE!!"
 
-echo "COMPLETE!!"
+# ##############################
+# Git
+# ##############################
+git flow init
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/eaglesakura/git-flow-hook/master/installer/install-hooks.sh)"
