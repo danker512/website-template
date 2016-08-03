60ba5977cf2221939fd59f2d12e203669deebd56
TSHiYK <toshiyuki.1223+dev@gmail.com>
2016-08-03 22:03:33 +0900

    chore(mkzip): 差分ファイルがうまく抽出できなかったのを修正。 refs #10

--- mkzip.sh
+++ mkzip.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
+
 echo 'All files? [y/n]'
 read all
+
+# ファイル一式の場合
 if [ $all = 'y' ]; then
-  git archive --format=zip HEAD:public -o ~/Desktop/public.zip
+  git archive --format=zip --prefix=public/ HEAD:public -o ~/Desktop/public.$(date +"%y%m%d%H%M").zip
+# 差分ファイルの場合
 elif [ $all = 'n' ]; then
   echo 'Enter commit ID'
-  read id
-
-  git archive --format=zip HEAD:public `git diff --diff-filter=D --name-only HEAD $id` -o ~/Desktop/public.zip
+  read commitID
+  git archive --format=zip --prefix=public/ HEAD:public $(git diff --diff-filter=D --name-only HEAD:public ${commitID}) -o ~/Desktop/public.$(date +"%y%m%d%H%M").zip
+# 不正な値が入力された場合
 else
   echo 'Invalid values.'
   exit 1
