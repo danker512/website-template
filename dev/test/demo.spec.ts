b29a2dcf9907c2559f2ba24ac162311d8272925f
TSHiYK <toshiyuki.i1223+dev@gmail.com>
2016-08-15 23:03:02 +0900

    fix(Demo): デモテストの修正

--- dev/test/demo.spec.ts
+++ dev/test/demo.spec.ts
@@ -5,7 +5,7 @@
 import { assert } from 'chai';
 import { Demo, Demo2 } from '../ts/demo';
 
-describe('Demoのテスト', ()=> {
+describe('Demoのテスト', () => {
   it('名前を取得する', () => {
     let demoClass = new Demo('TSHiYK');
     let name = demoClass.getName();
@@ -13,7 +13,7 @@ describe('Demoのテスト', ()=> {
   });
 });
 
-describe('Demo2のテスト', ()=> {
+describe('Demo2のテスト', () => {
 
   var demo2 = new Demo2();
 
@@ -26,5 +26,5 @@ describe('Demo2のテスト', ()=> {
     demo2.setName('TSHiYK');
     let name = demo2.getName();
     assert.equal(name, 'TSHiYK');
-  })
+  });
 });
